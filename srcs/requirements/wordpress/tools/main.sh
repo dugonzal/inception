be539f1db67da9fbcfce3ddf0eb02f274fbd0f6c
dugonzal <ciclocontinuo2@gmail.com>
2024-01-02 01:09:52 +0100

    vamos vamos he avanzado un monton solo faltaria el nginx

--- srcs/requirements/wordpress/tools/main.sh
+++ srcs/requirements/wordpress/tools/main.sh
@@ -8,13 +8,15 @@ while ! nc -z mariadb:3306 ; do
 	sleep 1;
 done
 
-wp core config --path=/var/www/html/wordpress --dbname=inception --dbuser=inception --dbhost=mariadb --dbpass=inception
-
-sleep 5
-
-wp core install --url=0.0.0.0:9000 --title="$DOMAIN_NAME" --admin_user="$MARIADB_USER" --admin_password="$MARIADB_PASSWORD" --admin_email="$EMAIL"
+if [ /var/www/html/wordpress/wp-config.php ] ;then
+    wp core config --path=/var/www/html/wordpress --dbname="$MARIADB_NAME" \
+      --dbuser="$MARIADB_USER" --dbhost="$MARIADB_HOST" --dbpass="$MARIADB_PASS" --extra-php <<PHP 
+      define( 'WP_DEBUG', true );
+      define( 'WP_DEBUG_LOG', true );
+PHP
+  sleep 5 
+  
+  wp core install --url="$DOMAIN_NAME" --title="$DOMAIN_NAME" --admin_user="$MARIADB_USER" --admin_password="$MARIADB_PASS" --admin_email="$EMAIL"
+fi 
 
 php81 -S 0.0.0.0:9000
-
-
-
