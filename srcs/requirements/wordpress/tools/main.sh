3dab0ad5981215eae3a6623b57d3a5b2ff321909
dugonzal <ciclocontinuo2@gmail.com>
2024-01-07 22:37:28 +0100

    acabo de ser capaz de servir mediante el proxy el contendido del servicio de wordpress

--- srcs/requirements/wordpress/tools/main.sh
+++ srcs/requirements/wordpress/tools/main.sh
@@ -6,17 +6,21 @@ while ! nc -z mariadb:3306 ; do
 	sleep 1;
 done
 
-if [ /var/www/html/wordpress/wp-config.php ] ;then
-    wp core config --path=/var/www/html/wordpress --dbname="$MARIADB_NAME" \
+if [ /var/www/wordpress/wp-config.php ] ;then
+    wp core config --path=/var/www/wordpress --dbname="$MARIADB_NAME" \
       --dbuser="$MARIADB_USER" --dbhost="$MARIADB_HOST" --dbpass="$MARIADB_PASS" --allow-root --extra-php <<PHP 
       define( 'WP_DEBUG', true );
       define( 'WP_DEBUG_LOG', true );
+      define('FS_METHOD', 'direct');
+      define('FS_CHMOD_DIR', 0775);
+      define('FS_CHMOD_FILE', 0664);
+
 PHP
   sleep 5 
   
   wp core install --url="$DOMAIN_NAME" --title="$DOMAIN_NAME" --admin_user="$MARIADB_USER" --admin_password="$MARIADB_PASS" --admin_email="$EMAIL" --allow-root
 fi 
+#mkdir -p /var/run/php-fpm81
+#php -S 0.0.0.0:9000
 
-php -S 0.0.0.0:9000
-
-#php-fpm81 -FR -c /etc/php81/conf.d/www.conf
+php-fpm81 -FR #-c /etc/php81/conf.d/www.conf
